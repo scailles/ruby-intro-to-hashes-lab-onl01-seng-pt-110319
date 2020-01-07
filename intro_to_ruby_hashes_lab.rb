68785841005d9096a4e656ee43609a651d60d711
Shaina C <scailles23@gmail.com>
2020-01-07 07:40:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-monopoly = {railroads:{}}
-monopoly[:railroads]={:pieces}
-monopoly[:railroads][:pieces]=4
+monopoly = {railroads:{:pieces=>4}}
 end
 
 
