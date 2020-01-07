402ec0277c1100241536714bdb6341b0a75be024
Shaina C <scailles23@gmail.com>
2020-01-07 07:59:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ monopoly =
       {
        reading_railroad:{},
        pennsylvania_railroad:{},
-       b_and_o_railroad: {}
+       b_and_o_railroad: {},
        shortline: {}
       },
     :rent_in_dollars
