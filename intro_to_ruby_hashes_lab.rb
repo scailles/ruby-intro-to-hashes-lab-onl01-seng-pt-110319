610c7e3d1e040a800b9ce8778bced486db120bdc
Shaina C <scailles23@gmail.com>
2020-01-07 07:44:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,15 @@ end
 
 
 def monopoly_with_third_tier
-
+monopoly = 
+{railroads:
+  {:pieces =>4,
+    :names =>{},
+    :rent_in_dollars => {
+      one_piece_owned: 25
+    }
+  }}
+end
 
 end
 
